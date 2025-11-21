8ac8d6a75570cb902a2c39b974ae1aa25565f351
Sesh <sergeimorozov212001@gmail.com>
2025-11-21 14:40:03 +0300

    tests(all)

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Для просмотра деплоя приложения можно перейти по следующим сслыкам
 --
-1. удаленный сервер: 
+1. удаленный сервер: http://samara.nomorepartiessbs.ru/
+2. github: https://sasha1339.github.io/react-burger/
--- cypress/integration/constructor/dnd.spec.js
+++ cypress/integration/constructor/dnd.spec.js
@@ -252,4 +252,74 @@ describe('service is available', function() {
     cy.contains('Детали ингредиента')
   });
 
+  it('should close modal window with ingredient', function() {
+    cy.get('[class^=IngredientUI_ingredient__]').first().as('productBun');
+
+
+    cy.get('@productBun')
+      .click()
+
+    cy.contains('Детали ингредиента')
+
+    cy.get('svg').first().as('closeButton');
+
+    cy.get('@closeButton').click({force: true});
+
+    cy.get('Детали ингредиента').should('not.exist');
+
+
+  });
+
+  it('should login', function() {
+    cy.get('[class^=IngredientUI_ingredient__]').eq(3).as('productSauce');
+    cy.get('[class^=IngredientUI_ingredient__]').first().as('productBun');
+    cy.get('[class^=BurgerConstructor_price__]').first().as('price');
+    cy.get('[class^=button ]').first().as('button');
+    cy.get('[class^=upBun]').first().as('upBun');
+    cy.get('[class^=BurgerConstructor_content__]').first().as('inner');
+
+
+    cy.get('@productSauce')
+      .trigger('dragstart');
+
+    cy.get('@inner')
+      .trigger('dragenter')
+      .trigger('dragover')
+      .trigger('drop');
+
+    // dragend на исходном элементе
+    cy.get('@productSauce')
+      .trigger('dragend');
+
+    cy.get('@productBun')
+      .trigger('dragstart');
+
+
+    cy.get('@upBun')
+      .trigger('dragenter')
+      .trigger('dragover')
+      .trigger('drop')
+      .trigger('dragend')
+
+    cy.get('@price')
+      .children()
+      .first()
+      .should('contain', '2590')
+
+    cy.get('@button')
+      .click()
+
+    cy.get('input').eq(0).type('samara@q1.q1');
+    cy.get('input').eq(1).type('samara');
+
+    cy.get('[class^=button ]').click();
+
+    cy.wait(3000);
+
+    cy.get('[class^=button ]').click();
+
+    cy.wait(20000);
+    cy.contains('идентификатор заказа')
+  });
+
 });
